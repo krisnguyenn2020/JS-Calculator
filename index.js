8f17030d1ea7c82a8cc89c98a7577be6a0a201ea
Kris Nguyen <krisnguyenn2020@gmail.com>
2022-08-12 16:16:14 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+// console.log(buttons);
+
+buttons.map( button => {
+            button.addEventListener('click',  (e) => {
+                        console.log('clicked');
+                        console.log(e.target);
+                        console.log(e.target.innerText);
+                        switch(e.target.innerText) {
+                                    case('C'):
+                                                display.innerText ="";
+                                                break;
+                                    case('←'):
+                                               display.innerText = display.innerText.slice(0,-1);
+                                                break;
+                                    case('='):
+                                                display.innerText = eval(display.innerText);
+                                                break;
+                                    default:
+                                                display.innerText += e.target.innerText;
+                        }
+            });
+});
